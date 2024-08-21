4e49630cbcaf4d1e1effb0f5206eef953c144796
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-21 17:56:12 -0500

    add method getAllUsers

--- apps/auth/src/services/userService.ts
+++ apps/auth/src/services/userService.ts
@@ -1,9 +1,19 @@
 import Util from "@/utils/util";
 
-export default class UserService {
-  async getAllUsers(endpoint: string): Promise<void> {
-    try {
-      const data = Util.fetchApi(endpoint, {});
-    } catch (error) {}
+export default class UserService { // Clase UserService
+  async getAllUsers(): Promise<void> { //---->Obtener todos los usuarios // Por el momento el retorno es void, debid a que aún no están las interfaces
+    try { // Manejador de eventos
+      return await Util.fetchApi("http://localhost:3000/users", {}); // Conexión con el endpoint . El método fetchApi ya valida los errores
+    } catch (error) {
+        console.log({message: "Error to get all users", error});
+    }
+  }
+
+  async getUserById():Promise<void>{
+    try{
+
+    }catch(){
+
+    }
   }
 }
