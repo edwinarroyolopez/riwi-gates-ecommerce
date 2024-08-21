61fa25955e48b501cb390d9a05269eec44a28685
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-21 18:27:03 -0500

    add method updatePassword

--- apps/auth/src/services/userService.ts
+++ apps/auth/src/services/userService.ts
@@ -1,13 +1,15 @@
+import IUser from "@/interfaces/userInterface";
 import Util from "@/utils/util";
 
 export default class UserService {
   // Clase UserService
-  async getAllUsers(): Promise<void> {
-    //---->Obtener todos los usuarios // Por el momento el retorno es void, debid a que aún no están las interfaces // Manejador de eventos
+  async getAllUsers(): Promise<IUser[]> {
+    // Obtener todos los users
     return await Util.fetchApi("http://localhost:3000/users", {}, "GET"); // Conexión con el endpoint . El método fetchApi ya valida los errores
   }
 
-  async getUserById(user_id: number): Promise<void> {
+  async getUserById(user_id: number): Promise<IUser> {
+    // Obtener un usuario por id
     // -----> Obtener un usuario por id
     return await Util.fetchApi(
       `http://localhost:3000/users/${user_id}`,
@@ -16,7 +18,8 @@ export default class UserService {
     );
   }
 
-  async createUser(user: Partial<>): Promise<void> {
+  async createUser(user: Partial<IUser>): Promise<IUser> {
+    // Crear un usuario
     // Esperando el tipo para indicarlo en el parametro ---->
     return await Util.fetchApi(
       "http://localhost:3000/users/",
@@ -31,9 +34,9 @@ export default class UserService {
     );
   }
 
-  async updateUser(user_id: number, user: Partial<>): Promise<void> {
+  async updateUser(user_id: number, user: Partial<IUser>): Promise<void> {
     // Esperando el tipo
-    return await Util.fetchApi(
+    await Util.fetchApi(
       `http://localhost:3000/users/${user_id}`,
       {
         method: "PUT",
@@ -45,7 +48,20 @@ export default class UserService {
       "PUT"
     );
   }
-  async updatePassword(user_id:number, newPassword: string):Promise<void>{
-    const user = await Util.
+  async updatePassword(user_id: number, newPassword: string): Promise<void> {
+    // Actualizar la password al usuario
+    await Util.fetchApi(
+      `http://localhost:3000/users/${user_id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          password: newPassword,
+        }),
+      },
+      "PATCH"
+    );
   }
 }
