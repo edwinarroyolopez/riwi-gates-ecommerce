6c7866be76583d9b6efa57f5984d05db21fd120e
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-21 18:02:49 -0500

    add method createUser

--- apps/auth/src/services/userService.ts
+++ apps/auth/src/services/userService.ts
@@ -1,19 +1,35 @@
 import Util from "@/utils/util";
 
 export default class UserService { // Clase UserService
-  async getAllUsers(): Promise<void> { //---->Obtener todos los usuarios // Por el momento el retorno es void, debid a que aún no están las interfaces
-    try { // Manejador de eventos
-      return await Util.fetchApi("http://localhost:3000/users", {}); // Conexión con el endpoint . El método fetchApi ya valida los errores
-    } catch (error) {
-        console.log({message: "Error to get all users", error});
-    }
+  async getAllUsers(): Promise<void> { //---->Obtener todos los usuarios // Por el momento el retorno es void, debid a que aún no están las interfaces // Manejador de eventos
+    return await Util.fetchApi("http://localhost:3000/users", {}); // Conexión con el endpoint . El método fetchApi ya valida los errores
   }
 
-  async getUserById():Promise<void>{
-    try{
+  async getUserById(user_id:number):Promise<void>{ // -----> Obtener un usuario por id
+    return await Util.fetchApi(`http://localhost:3000/users/${user_id}`, {})
+  }
 
-    }catch(){
-
-    }
+  async createUser(user:){ // Esperando el tipo para indicarlo en el parametro ---->
+    const {name,document_number, email, password,birthdate, 
+            cellphone, zip_code, address, type_document_id, country_id, role_id} = user;
+    return await Util.fetchApi("http://localhost:3000/users/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application-json"
+        },
+        body: JSON.stringify({
+            name,
+            document_number,
+            email,
+            password,
+            birthdate,
+            cellphone,
+            zip_code,
+            address,
+            type_document_id,
+            country_id,
+            role_id
+        })
+    })
   }
 }
