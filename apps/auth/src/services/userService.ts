d94662c819048a49f0ba05830ce2d64ee6d4e341
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-21 17:50:06 -0500

    add endpoints

--- apps/auth/src/services/userService.ts
+++ apps/auth/src/services/userService.ts
@@ -1,1 +1,9 @@
-export default class UserService {}
+import Util from "@/utils/util";
+
+export default class UserService {
+  async getAllUsers(endpoint: string): Promise<void> {
+    try {
+      const data = Util.fetchApi(endpoint, {});
+    } catch (error) {}
+  }
+}
