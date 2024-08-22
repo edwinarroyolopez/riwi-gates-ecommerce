a2a57bda416b697b6d12d0eca83162ce6d05c28d
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-21 19:02:40 -0500

    add fetchApi

--- apps/auth/src/services/userService.ts
+++ apps/auth/src/services/userService.ts
@@ -1,11 +1,43 @@
 import IUser from "@/interfaces/userInterface";
-import Util from "@/utils/util";
 
 export default class UserService {
+  static async fetchApi(
+    // Este método nos sirve para reutilizar tanto su manejador de erores como el fetch
+    url: string,
+    options: {},
+    typeVerb: string,
+    nameMethod: string
+  ) {
+    // Url: endoint,
+    // options: options del método,
+    // typeVerb: El tipo de verbo que son GET, POST, UPDATE, DELETE,
+    // nameMethod: Nombre del método, ya sea getUsers, deleteUser - Esto se utiliza para mostrar en el manejador de errores
+    try {
+      // Manejador de errores
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        // Si la respuesta no es lo que se esperaba lanzar un nuevo error y el status
+        throw new Error(`Error with the response - Status: ${response.status}`);
+      }
+      return response.json(); // Se espera retornar la respuesta en formato json
+    } catch (error) {
+      // Caso de error mostrar
+      console.log({
+        message: `Error with the method ${nameMethod}  - verb ${typeVerb}. Try again!`,
+        error,
+      });
+    }
+  }
+
   // Clase UserService
   async getAllUsers(): Promise<IUser[]> {
     // Obtener todos los users
-    return await Util.fetchApi("http://localhost:3000/users", {}, "GET"); // Conexión con el endpoint . El método fetchApi ya valida los errores
+    return await UserService.fetchApi(
+      "http://localhost:3000/users",
+      {},
+      "GET",
+      "getAllUsers"
+    ); // Conexión con el endpoint . El método fetchApi ya valida los errores
   }
 
   async getUserById(user_id: number): Promise<IUser> {
