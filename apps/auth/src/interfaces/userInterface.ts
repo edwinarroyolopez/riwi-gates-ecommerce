7e2d1fe3466208321da69ade912994797240d789
joseCardona01 <josesiprozmaster@gmail.com>
2024-08-23 01:29:43 -0500

    add class UserService

--- apps/auth/src/interfaces/userInterface.ts
+++ apps/auth/src/interfaces/userInterface.ts
@@ -1,14 +1,8 @@
 export default interface IUser {
-  id: number;
-  name: string;
-  document_number: number;
-  email: string;
-  password: string;
-  birthdate: string;
-  cellphone: string;
-  zip_code: string;
-  address: string;
-  type_document_id: number;
-  country_id: number;
-  role_id: number;
+  name: string,
+  email:string,
+  password: string,
+  phone: string,
+  adress: string,
+  roles: object[]
 }
