d145a6a91c0769aaa988082e3efd52d662b8f794
mariaisva2 <mariac20160@gmail>
2024-08-23 09:35:08 -0500

    LookSection

--- apps/customer/src/app/home/page.tsx
+++ apps/customer/src/app/home/page.tsx
@@ -3,6 +3,8 @@ import Navbartop from '../components/ui/navbar/navbartop';
 import Hero from '../components/ui/hero/hero';
 import Categorymain from '../components/cards/categorymain';
 import Categoryseason from '../components/cards/categoryseason';
+import LookSection from '../components/LookSection/LookSection';
+import styles from '../components/LookSection/styles/LookSection.module.scss'
 
 const Category: FC = () => {
   return (
@@ -11,6 +13,7 @@ const Category: FC = () => {
         <Hero/>
         <Categorymain/>
         <Categoryseason/>
+        <LookSection />
       </main>
   );
 };
